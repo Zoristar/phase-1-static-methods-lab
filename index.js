4e48fbcf7c68d558e15d7ef24ea26a772789b2da
Zoriana <zoriana.shatkovska@gmail.com>
2022-12-01 01:38:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return `${string[0].toUpperCase()}${string.slice(1)}`
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    const someString = string.split(' ')
+    let results = []
+    const except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (const word of someString) {
+      results.push(except.includes(word) ? word : capital(word))
+    }
+    
+    return capital(results.join(" "))
+  }
+}
+
+function capital (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
